f54198042d302044f25d46660881f78c7b22a0de
Marius Fersigan <marius.fersigan@gmail.com>
2021-09-02 03:38:44 +0300

    update

---
+++ OHLCConverter/OHLCConverter/Converter.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OHLCConverter
+{
+    public record OHLC()
+    {
+        public decimal Open { get; init; }
+        public decimal High { get; init; }
+        public decimal Low { get; init; }
+        public decimal Close { get; init; }
+        public decimal Volume { get; init; }
+        public DateTime Date { get; init; }
+        public TimeSpan OpenTime { get; init; }
+
+        public string CSVRow(string separator = ",")
+        {
+            return
+                String.Join(separator, new string[] {
+                    this.Date.ToString("yyyyMMdd"),
+                    this.OpenTime.ToString("hmm"),
+                    this.Open.ToString(),
+                    this.High.ToString(),
+                    this.Low.ToString(),
+                    this.Close.ToString(),
+                    this.Volume.ToString()
+                });
+        }
+    }
+
+    class Converter
+    {
+        // relative small number of candles/bars with timeframe > 1 minute (the resizing of when append whould not be an issue).
+        List<OHLC> chart = new();
+        
+        public readonly TimeSpan timeframe;
+        public readonly TimeSpan sessionStart;
+        public readonly TimeSpan sessionEnd;
+
+        DateTime date;
+        TimeSpan openTime;
+        decimal tOpen;
+        decimal tHigh;
+        decimal tLow;
+        decimal tClose;
+        bool startNewBar = true;
+
+        
+        
+        
+        public Converter(TimeSpan timeframe, TimeSpan sessionStart, TimeSpan sessionEnd)
+        {
+            this.timeframe = timeframe;
+            this.sessionStart = sessionStart;
+            this.sessionEnd = sessionEnd;
+        }
+
+        public void Convert(decimal m1Open, decimal m1High, decimal m1Low, decimal m1Close, DateTime m1Date, TimeSpan m1Time)
+        {
+            if (startNewBar)
+            {
+                this.openTime = m1Time;
+
+            }
+        }
+
+
+
+
+
+    }
+}
---
+++ OHLCConverter/OHLCConverter/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading.Tasks;
+using Sylvan.Data;
+
+namespace OHLCConverter
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Start Testing:");
+
+            var p = new Program();
+
+            await p.TestCSV(@"D:\order_157717.tar\order_157717\table_mxim.csv");
+            Console.WriteLine("Finished");
+
+            //OHLC r = new() { Open = 10m; High = 10 };
+            OHLC r = new()
+            {
+                Open = 10m,
+                Close = 10m
+            };
+
+            Console.ReadKey();           
+            
+        }
+        // @"D:\order_157717.tar\order_157717\table_mxim.csv"
+        public async Task TestCSV(string path)
+        {
+            int count = 0;
+            using var csv = await Sylvan.Data.Csv.CsvDataReader.CreateAsync(path);
+
+            while (await csv.ReadAsync())
+            {
+                var id = csv.GetString(0);
+                var name = csv.GetString(1);
+                var o = csv.GetDecimal(2);
+                var h = csv.GetDecimal(3);
+                var l = csv.GetDecimal(4);
+                var c = csv.GetDecimal(5);
+                //Console.WriteLine("Count is {0} and volume is {1}", count, csv.GetString(6));
+                var v = Decimal.Parse(csv.GetString(6), System.Globalization.NumberStyles.Float);
+                count++;
+            }
+
+            Console.WriteLine("Count is: {0}", count);
+
+        }
+    }
+
+        
+}

0c900b127cf4e7b09be2c5799712d36724da4f87
Marius Fersigan <marius.fersigan@gmail.com>
2021-09-03 07:08:46 +0300

    update

--- OHLCConverter/OHLCConverter/Converter.cs
+++ OHLCConverter/OHLCConverter/Converter.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Common;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
+using Sylvan.Data.Csv;
 
 namespace OHLCConverter
 {
@@ -16,6 +20,10 @@ namespace OHLCConverter
         public DateTime Date { get; init; }
         public TimeSpan OpenTime { get; init; }
 
+        public decimal Extra1 { get; init; }
+        public decimal Extra2 { get; init; }
+        public decimal Extra3 { get; init; }
+
         public string CSVRow(string separator = ",")
         {
             return
@@ -26,11 +34,17 @@ namespace OHLCConverter
                     $"{High}",
                     $"{Low}",
                     $"{Close}",
-                    $"{Volume}"                    
+                    $"{Volume}",
+                    $"{Extra1}",
+                    $"{Extra2}",
+                    $"{Extra3}"
                 });
         }
     }
 
+    /// <summary>
+    /// Converter class containing the logic for converting 1-minute charts to NN minutes timeframes.
+    /// </summary>
     class Converter
     {
         // relative small number of candles/bars with timeframe > 1 minute (the resizing of when append whould not be an issue).
@@ -43,9 +57,14 @@ namespace OHLCConverter
         DateTime _openDate;
         TimeSpan _openTime;
         decimal _open;
-        decimal _high;
-        decimal _low;
-        decimal _close;     
+        decimal _high = Decimal.MinValue;
+        decimal _low = Decimal.MaxValue;
+        decimal _close;
+        decimal _volume = 0;
+        bool init = false;
+        decimal _extra1;
+        decimal _extra2;
+        decimal _extra3;
         
         
         public Converter(int nnTarget, TimeSpan sessionStart, TimeSpan sessionEnd)
@@ -55,30 +74,121 @@ namespace OHLCConverter
             _sessionEnd = sessionEnd;            
         }
 
-        public void Convert(decimal m1Open, decimal m1High, decimal m1Low, decimal m1Close, DateTime m1Date, TimeSpan m1Time)
+        /// <summary>
+        /// Receives a stream of 1-minute OHLC bars - to avoid loading the entire datasets in memory.
+        /// This method contains the main logic for NN timeframe bars generation from 1-minute bars.
+        /// </summary>
+        /// <param name="m1Bar"></param>
+        public void Convert(OHLC m1Bar)
         {
-            var minutesOffset = m1Time.Minutes;
+            if (!inSession(m1Bar))
+                return;
             
-            if (isNewBar())
+            var minutesOffset = m1Bar.OpenTime.Minutes;
+            
+            if (isNewBar(m1Bar))
             {
-                _openTime = new(m1Time.Hours, minutesOffset - minutesOffset % _timeFrame, 0);
-                
+                if (init)
+                {
+                    AddCurrentBar();
+                }
 
+                _volume = 0;
+                _high = Decimal.MinValue;
+                _low = Decimal.MaxValue;
+                _openTime = new TimeSpan(m1Bar.OpenTime.Hours, minutesOffset - minutesOffset % _timeFrame, 0);
+                _openDate = m1Bar.Date;
+                _open = m1Bar.Open;
+
+                _extra1 = m1Bar.Extra1;
+                _extra2 = m1Bar.Extra2;
+                _extra3 = m1Bar.Extra3;
+
+                init = true;
             }
 
-            bool isNewBar()
+            _high = Math.Max(m1Bar.High, _high);
+            _low = Math.Min(m1Bar.Low, _low);
+            _close = m1Bar.Close;
+            _volume += m1Bar.Volume;
+            _extra1 = Math.Max(m1Bar.Extra1, _extra1);
+            _extra2 = Math.Max(m1Bar.Extra2, _extra2);
+            _extra3 = Math.Max(m1Bar.Extra3, _extra3);
+
+
+            bool isNewBar(OHLC m1Bar)
             {
                 return 
-                    m1Time.Subtract(_openTime).TotalMinutes > _timeFrame
-                    || m1Date > _openDate;   
+                    m1Bar.OpenTime.Subtract(_openTime).TotalMinutes > _timeFrame
+                    || m1Bar.Date > _openDate;                  
                 
+            }
+
+            
+               
+            // Added support for all possible time intervals.
+            // For example: session starting at 10pm and session ending at 6am.
+            // This means including everything after 6am and before 10pm.               
+            bool inSession(OHLC m1Bar)
+            {
+                if (_sessionStart < _sessionEnd)
+                {
+                    return m1Bar.OpenTime >= _sessionStart && m1Bar.OpenTime < _sessionEnd;
+                } else if (_sessionStart > _sessionEnd)
+                {
+                    return m1Bar.OpenTime >= _sessionStart || m1Bar.OpenTime < _sessionEnd;
+                }
                 
+                // _sessionStart == sessionEnd - treated as 24h session
+                return true;
             }
         }
 
+        /// <summary>
+        /// Add wip (and finalized) bar to chart list.
+        /// </summary>
+        private void AddCurrentBar()
+        {
+            OHLC bar = new()
+            {
+                Date = _openDate,
+                OpenTime = _openTime,
+                Open = _open,
+                High = _high,
+                Low = _low,
+                Close = _close,
+                Volume = _volume,
+                Extra1 = _extra1,
+                Extra2 = _extra2,
+                Extra3 = _extra3
+            };
 
+            chart.Add(bar);
+        }
 
+        
+        /// <summary>
+        /// Adding the last potentially unfinished/wip bar to chart.
+        /// Called internally before writing/exporting the generated data.
+        /// </summary>
+        private void Flush()
+        {
+            if (init)
+            {
+                AddCurrentBar();
+                init = false;
+            }           
+        }
 
-
+        /// <summary>
+        /// Write the generated NN bar to CSV file.
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public async Task WriteCSVAsync(string path)
+        {
+            Flush();
+            await File.WriteAllLinesAsync(path, chart.Select(x => x.CSVRow()));                      
+        }
     }
 }
--- OHLCConverter/OHLCConverter/Program.cs
+++ OHLCConverter/OHLCConverter/Program.cs
@@ -1,55 +1,158 @@
 ﻿using System;
 using System.Globalization;
 using System.Threading.Tasks;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.IO;
 using Sylvan.Data;
 
 namespace OHLCConverter
 {
     class Program
-    {
-        static async Task Main(string[] args)
+    {        
+        static async Task<int> Main(string[] args)
         {
-            Console.WriteLine("Start Testing:");
-
-            var p = new Program();
-
-            await p.ConvertCSV(@"D:\order_157717.tar\order_157717\table_mxim.csv");
-            Console.WriteLine("Finished");
-
-            //OHLC r = new() { Open = 10m; High = 10 };
-            OHLC r = new()
+            var rootCommand = new RootCommand
             {
-                Open = 10m,
-                Close = 10m
-            };
+                new Option<int>(
+                    "--timeframe",
+                    getDefaultValue: () => 30,
+                    description: "New target timeframe value (in minutes)."),
+                new Option<bool>(
+                    "--batch-mode",
+                    getDefaultValue: () => false,
+                    description: "Running the convertor in batch mode (need to specify the source folder)"),
+                new Option<FileInfo>(
+                    "--source-dir",
+                    "Directory containing the CSV files."),
+                new Option<FileInfo>(
+                    "--target-dir",
+                    "Directory to contain converted CSV files (it will be created if does not exist)."),
+                new Option<FileInfo>(
+                    "--source-file",
+                    "CSV file to convert."),
+                new Option<FileInfo>(
+                    "--target-file",
+                    "CSV path for the converted file (default: saved locally using the symbol$INTRADAYNN.csv pattern)."),
+                new Option<TimeSpan>(
+                    "--session-start",
+                    "Session start time: e. g. 9:30"),
+                new Option<TimeSpan>(
+                    "--session-end",
+                    "Session end time: e. g. 16:00")
+            };            
 
-            Console.ReadKey();           
-            
+            rootCommand.Description = "OHLC bar convertor";
+            var p = new Program();
+            rootCommand.Handler = 
+                CommandHandler.Create<int, bool, FileInfo, FileInfo, FileInfo, FileInfo, TimeSpan, TimeSpan>(
+                    async (timeFrame, batchMode, sourceDir, targetDir, sourceFile, targetFile, sessionStart, sessionEnd) =>
+                    {
+                        Console.WriteLine($"--timeframe is: {timeFrame}");
+                        Console.WriteLine($"--batch-mode is: {batchMode}");
+                        Console.WriteLine($"--source-dir is: {sourceDir?.FullName ?? null}");
+                        Console.WriteLine($"--target-dir is: {targetDir?.FullName ?? null}");
+                        Console.WriteLine($"--source-file is: {sourceFile?.FullName ?? null}");
+                        Console.WriteLine($"--target-file is: {targetFile?.FullName ?? null}");
+                        Console.WriteLine($"--session-start is: {sessionStart}");
+                        Console.WriteLine($"--session-end is: {sessionEnd}");
+                        Console.WriteLine();
+
+                        if (batchMode)
+                        {
+                            if (sourceDir is not null && targetDir is not null)
+                            {
+                                if (!Directory.Exists(sourceDir.FullName))
+                                {
+                                    Console.WriteLine($"--source-dir value is not referring to an existing directory: {sourceDir.FullName}");
+                                }
+                                else
+                                {
+                                    if (!targetDir.Exists) Directory.CreateDirectory(targetDir.FullName);
+
+                                    var csvFiles = Array.FindAll(Directory.GetFiles(sourceDir.FullName), path => Path.GetExtension(path) == ".csv");
+
+                                    foreach (var f in csvFiles)
+                                    {
+                                        Console.WriteLine($"Processing {f}");
+                                        
+                                        await p.ConvertCSV(f, timeFrame, sessionStart, sessionEnd, targetDir: targetDir.FullName);
+                                    }    
+                                }   
+                            } 
+                            else
+                            {
+                                Console.WriteLine("Please check --source-dir and --target-dir values!");
+                            }                  
+                        } 
+                        else
+                        {
+                            if (sourceFile is not null && sourceFile.Exists)
+                            {
+                                Console.WriteLine($"Processing {sourceFile.FullName}");                                
+                                await p.ConvertCSV(sourceFile.FullName, timeFrame, sessionStart, sessionEnd, targetFile?.FullName ?? "");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Please check and use a valid --source-file argument!");
+                            }
+                        }
+                    });
+
+            return rootCommand.InvokeAsync(args).Result;
+     
+
         }
-        // @"D:\order_157717.tar\order_157717\table_mxim.csv"
-        public async Task ConvertCSV(string path)
+        
+        public async Task ConvertCSV(string sourcePath, int timeframe, TimeSpan sessionStart, TimeSpan sessionEnd, string targetPath = "", string targetDir = "")
         {
-            using var csv = await Sylvan.Data.Csv.CsvDataReader.CreateAsync(path);            
+            Converter converter = new(timeframe, sessionStart, sessionEnd);
 
+            // Sylvan.Data CSV reader is an extremely fast reader using the SIMD approach.
+            using var csv = await Sylvan.Data.Csv.CsvDataReader.CreateAsync(sourcePath);            
+            
             while (await csv.ReadAsync())
+            {
+                converter.Convert(readBar());
+            }
+
+            await converter.WriteCSVAsync(makeTargetFileName());
+
+            OHLC readBar()
             {
                 var date = dateTimeParser(csv.GetString(0));
                 var time = timeParser(csv.GetString(1));
                 var o = csv.GetDecimal(2);
                 var h = csv.GetDecimal(3);
                 var l = csv.GetDecimal(4);
-                var c = csv.GetDecimal(5);
+                var c = csv.GetDecimal(5);               
                 
+
                 // Some volume values are represented by scientific notation in the source files,
                 // using NumberStyles.Float fixes the parsing issue.
                 // Also, there are volume values that are represented as floating points so I decided to go
                 // with deciamal (integer is not a valid option anyway) instead of float to avoid precision related issues.
-                var vol = Decimal.Parse(csv.GetString(6), NumberStyles.Float);                
-            }
+                var vol = Decimal.Parse(csv.GetString(6), NumberStyles.Float);
+                
+                var extra1 = csv.GetDecimal(7);      
+                var extra2 = csv.GetDecimal(8);
+                var extra3 = csv.GetDecimal(9);
 
+                return new()
+                {
+                    Date = date,
+                    OpenTime = time,
+                    Open = o,
+                    High = h,
+                    Low = l,
+                    Close = c,
+                    Volume = vol,
+                    Extra1 = extra1,
+                    Extra2 = extra2,
+                    Extra3 = extra3
+                };
+            }            
             
-
-
             // helper local functions for custom dataformat and time parsing
             // just for readability purpose             
             DateTime dateTimeParser(string input)
@@ -60,6 +163,24 @@ namespace OHLCConverter
             TimeSpan timeParser(string input)
             {
                 return TimeSpan.ParseExact(input.PadLeft(4, '0'), "hhmm", CultureInfo.InvariantCulture);
+            }
+
+            string makeTargetFileName()
+            {
+                var symbol = Path.GetFileNameWithoutExtension(sourcePath).Split("_")[1];
+                var fileName = $"{symbol.ToUpper()}$INTRADAY{timeframe}.csv";
+
+                if (!String.IsNullOrEmpty(targetDir))
+                {                    
+                    return Path.Combine(targetDir, fileName);
+                }
+                else if (!String.IsNullOrEmpty(targetPath))
+                {
+                    return targetPath;
+                }
+
+                return fileName;                
+                
             }
         }
     }
